3da842de8ac2b017435768352276a859ec6b6de1
Bertram Aakjær <89650492+BertramAakjaer@users.noreply.github.com>
2025-10-13 16:52:57 +0200

    test

--- include/parse.h
+++ include/parse.h
@@ -20,6 +20,6 @@ struct employee_t {
 
 int create_db_header(int fd, struct dbheader_t **headerOut);
 int validate_db_header(int fd, struct dbheader_t **headerOut);
-int output_file(int fd, struct dbheader_t *dbhrd struct employee_t *employees);
+int output_file(int fd, struct dbheader_t *dbhrd, struct employee_t *employees);
 
 #endif
